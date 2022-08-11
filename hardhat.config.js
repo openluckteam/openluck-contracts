b737934f058265bf82bebf6f839b50b9c86271a3
ToniDoge <bugdogeman@gmail.com>
2022-08-11 09:30:07 +0800

    polygon deployments

--- hardhat.config.js
+++ hardhat.config.js
@@ -165,7 +165,7 @@ module.exports = {
     "eth": {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privatekeys("eth"),
-      gasPrice: 20000000000, //20Gwei
+      gasPrice: 10000000000, //10Gwei
       network_id: 1,
       timeout: 600000,
     },
